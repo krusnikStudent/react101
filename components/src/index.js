2552430db2fb952e255b964a3423a3aa7c913ced
csevere <carla.severe@gmail.com>
2019-04-18 21:09:49 -0400

    components and props refresh

---
+++ components/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'; 
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard >
+                <CommentDetail author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    avatar={faker.image.avatar()} 
+                    comments="So rad, dude!" 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:01AM" 
+                    avatar={faker.image.avatar()} 
+                    comments="Awesome blog post!" 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 3:03PM" 
+                    avatar={faker.image.avatar()} 
+                    comments="When's the next one?" 
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDom.render(<App />, document.querySelector('#root'));
